67211f8d78fc6bd595f0f5129de04140cf6be687
Jared-Lim <jaredlgw@outlook.com>
2016-11-15 10:21:27 +1100

    first variant for output

--- convert.pl
+++ convert.pl
@@ -1,9 +1,33 @@
 #!/usr/bin/perl -w
 
+use Time::HiRes qw(usleep nanosleep);
+
+if(-e "output"){
+}else{
+	mkdir "output";
+	print "created output directory\n";
+}
+sleep(1);
+
+$count = 1;
+
+print "--------------------------\n";
+print "\tSkill ".$count."\n";
+print "--------------------------\n";
+
 while(<>){
+	chomp $_;
 	if($_ =~ /registerSkill/){
-		print "HAHAHAHA\n";
+		print "{\n";
+	}elsif($_ =~ /\}\)\;/){
+		print "}\n";
+	}elsif($_ =~ /^\s$/){
+		$count++;
+		print "--------------------------\n";
+		print "\tSkill ".$count."\n";
+		print "--------------------------\n";
 	}else{
-		print "1";
+		print $_."\n";
 	}
+	usleep(100000);
 }
